39439d6a1f44de0f914a1d5c42eff28817809e7c
BuickSalesman <adam.odonnell.e@gmail.com>
2024-05-06 17:33:25 -0400

    Add create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,15 @@ class GamesController < ApplicationController
     @games = Game.all
     render :index
   end
+
+  def create
+    @game = Game.create(
+      title: params[:title],
+      image: params[:image],
+      description: params[:description],
+      release_date: params[:release_date],
+      platform: params[:platform],
+    )
+    render :show
+  end
 end
--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -8,4 +8,17 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Game.count, data.length
   end
+
+  test "create" do
+    assert_difference "Game.count", 1 do
+      post "/games.json", params: {
+                            title: "lake",
+                            image: "lake",
+                            description: "lake",
+                            release_date: "lake",
+                            platform: "lake",
+                          }
+      assert_response 200
+    end
+  end
 end
